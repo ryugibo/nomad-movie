b9c5c70c008b8e049e67361f0ed5682be304dd05
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-02-01 18:14:09 +0900

    :bookmark: 224 | Navigation 전체 페이지에 링크 추가

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import "./App.css";
 import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import About from "./routes/About";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
     <HashRouter>
+      <Navigation />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
