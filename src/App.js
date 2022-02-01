d4a4e79e3c4742bc6afc1258d31f9d41a5e47390
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-02-01 15:47:59 +0900

    :bookmark: 100 | 에러메시지 처리

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-const foodILIke = [
+const foodILike = [
   {
+    id: 1,
     name: "김치",
     image: "https://1.bp.blogspot.com/-ss_pbo3ovUY/VCka72oQx9I/AAAAAAAAm-I/Jsz5t-52JCc/s400/food_kimuchi.png",
   },
   {
+    id: 2,
     name: "삼겹살",
     image: "https://1.bp.blogspot.com/-vlM-aCacrPM/X1CK6FoXzlI/AAAAAAABawM/y_rt9HUgMAMomLmeL-T97UXQX9c-VLH4QCNcBGAsYHQ/s400/food_yakiniku_samugyopusaru.png",
   },
   {
+    id: 3,
     name: "라면",
     image: "https://3.bp.blogspot.com/-rZMHLcW6Er4/WlGpO69KN7I/AAAAAAABJlg/KgmrOkMSuoM0Xf0qRil3iOpMlGer-ypmACLcBGAs/s400/food_cup_ramen_miso.png",
   },
   {
+    id: 4,
     name: "돈까스",
     image: "https://1.bp.blogspot.com/-ndPI4wq1sRs/X3hF_nt2teI/AAAAAAABbmU/r8d5zTyOs0oITNVtj-p76sXoDPUtXwi5QCNcBGAsYHQ/s400/food_tonkatsu_solo.png",
   },
   {
+    id: 5,
     name: "김밥",
     image: "https://1.bp.blogspot.com/-y3daFCAqXxI/UZRBfkzM0DI/AAAAAAAASnk/gPg7Uns_xmM/s400/setsubun_ehomaki3.png",
   },
@@ -25,7 +30,7 @@ function Food({ name, picture }) {
   return (
     <div>
       <h2>{ name } 좋아</h2>
-      <img src={picture} alt="" />
+      <img src={ picture } alt={ name } />
     </div>
   );
 }
@@ -33,7 +38,7 @@ function Food({ name, picture }) {
 function App() {
   return (
     <div>
-      { foodILIke.map(dish => (<Food name={dish.name} picture={dish.image} />)) }
+      { foodILike.map(dish => (<Food key={ dish.id } name={ dish.name } picture={ dish.image } />)) }
     </div>
   )
 }
