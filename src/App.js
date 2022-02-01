3548a425523db4722d2543c0570ecab1e3868102
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-02-01 16:11:23 +0900

    :bookmark: 128 | state를 활용한 페이지 구성
    - state 갱신은 setState 함수를 통해서 해야함.

--- src/App.js
+++ src/App.js
@@ -1,60 +1,31 @@
-import PropTypes from 'prop-types';
+import React from "react";
 
-const foodILike = [
-  {
-    id: 1,
-    name: "김치",
-    image: "https://1.bp.blogspot.com/-ss_pbo3ovUY/VCka72oQx9I/AAAAAAAAm-I/Jsz5t-52JCc/s400/food_kimuchi.png",
-    rating: 3,
-  },
-  {
-    id: 2,
-    name: "삼겹살",
-    image: "https://1.bp.blogspot.com/-vlM-aCacrPM/X1CK6FoXzlI/AAAAAAABawM/y_rt9HUgMAMomLmeL-T97UXQX9c-VLH4QCNcBGAsYHQ/s400/food_yakiniku_samugyopusaru.png",
-    rating: 4,
-  },
-  {
-    id: 3,
-    name: "라면",
-    image: "https://3.bp.blogspot.com/-rZMHLcW6Er4/WlGpO69KN7I/AAAAAAABJlg/KgmrOkMSuoM0Xf0qRil3iOpMlGer-ypmACLcBGAs/s400/food_cup_ramen_miso.png",
-    rating: 4.3,
-  },
-  {
-    id: 4,
-    name: "돈까스",
-    image: "https://1.bp.blogspot.com/-ndPI4wq1sRs/X3hF_nt2teI/AAAAAAABbmU/r8d5zTyOs0oITNVtj-p76sXoDPUtXwi5QCNcBGAsYHQ/s400/food_tonkatsu_solo.png",
-    rating: 5,
-  },
-  {
-    id: 5,
-    name: "김밥",
-    image: "https://1.bp.blogspot.com/-y3daFCAqXxI/UZRBfkzM0DI/AAAAAAAASnk/gPg7Uns_xmM/s400/setsubun_ehomaki3.png",
-    rating: 4.5,
-  },
-];
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
 
-function Food({ name, picture, rating }) {
-  return (
-    <div>
-      <h2>{ name } 좋아</h2>
-      <h4>{ rating }/5.0</h4>
-      <img src={ picture } alt={ name } />
-    </div>
-  );
+  add = () => {
+    this.setState(current => ({
+      count: current.count + 1,
+    }));
+  };
+
+  minus = () => {
+    this.setState(current => ({
+      count: current.count - 1,
+    }));
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>숫자: { this.state.count }</h1>
+        <button onClick={ this.add }>더하기</button>
+        <button onClick={ this.minus }>빼기</button>
+      </div>
+    );
+  }
 }
-
-function App() {
-  return (
-    <div>
-      { foodILike.map(dish => (<Food key={ dish.id } name={ dish.name } picture={ dish.image } rating={ dish.rating } />)) }
-    </div>
-  )
-}
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number,
-};
 
 export default App;
