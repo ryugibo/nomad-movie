d07d41de61081031876a494795058d129f7053b3
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-02-01 15:29:20 +0900

    :bookmark: 88 | 배열에 저장된 음식 목록을 사용

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
-function Food({ fav }) {
-  return <h1>{ fav } 좋아</h1>;
+const foodILIke = [
+  {
+    name: "김치",
+    image: "",
+  },
+  {
+    name: "삼겹살",
+    image: "",
+  },
+  {
+    name: "비빔밥",
+    image: "",
+  },
+  {
+    name: "돈까스",
+    image: "",
+  },
+  {
+    name: "김밥",
+    image: "",
+  },
+];
+
+function Food({ name }) {
+  return <h1>{ name } 좋아</h1>;
 }
 
 function App() {
   return (
     <div>
-      <h1>안녕</h1>
-      <Food fav="김치" />
-      <Food fav="라면" />
-      <Food fav="삼겹살" />
-      <Food fav="쭈꾸미" />
+      { foodILIke.map(dish => (<Food name={dish.name} />)) }
     </div>
   )
 }
