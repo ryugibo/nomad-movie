e247080a7e031cfc5c6532e9ab2caf83d9cd0eda
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-03-03 23:05:19 +0900

    :sparkle: useEffect 워닝 제거

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,20 +1,20 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import { useParams } from "react-router-dom";
 import Movie from "../components/Movie";
 
 function Detail() {
   const { id } = useParams();
   const [movie, setMovie] = useState({});
-  const getMovie = async () => {
+  const getMovie = useCallback(async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
     setMovie(json.data.movie);
-  };
+  }, [id]);
   console.log(movie);
   useEffect(() => {
     getMovie();
-  }, []);
+  }, [getMovie]);
   return Object.keys(movie).length === 0 ? (
     <h1>Loading...</h1>
   ) : (
