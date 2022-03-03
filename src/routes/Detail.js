e56a6a9ef5929bcd25dd3711e9ba48d762337fe8
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-03-03 22:57:50 +0900

    :pencil2: Detail 페이지에 영화 정보 표시

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,17 +1,31 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import Movie from "../components/Movie";
+
 function Detail() {
   const { id } = useParams();
+  const [movie, setMovie] = useState({});
   const getMovie = async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
-    console.log(json);
+    setMovie(json.data.movie);
   };
+  console.log(movie);
   useEffect(() => {
     getMovie();
   }, []);
-  return <h1>Detail</h1>;
+  return Object.keys(movie).length === 0 ? (
+    <h1>Loading...</h1>
+  ) : (
+    <Movie
+      id={movie.id}
+      coverImg={movie.medium_cover_image}
+      title={movie.title}
+      summary={movie.description_full}
+      genres={movie.genres}
+    />
+  );
 }
 
 export default Detail;
