ce8c2a9e042117ce01608de0eb0f4cc2e6a9d49d
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-03-03 20:45:47 +0900

    :pencil2: 코드 챌린지
    - 달러를 환전하는 기능

--- src/App.js
+++ src/App.js
@@ -3,29 +3,54 @@ import { useEffect, useState } from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [selectedCoin, setSelectedCoin] = useState();
+  const [usd, setUsd] = useState("");
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
       .then((response) => response.json())
       .then((json) => {
+        setSelectedCoin(json[0].id);
         setCoins(json);
         setLoading(false);
       });
   }, []);
+  const onChangeUsd = (event) => {
+    setUsd(event.target.value);
+  };
+  const onChangeSelect = (event) => {
+    setSelectedCoin(event.target.value);
+  };
   return (
     <div>
       <h1>The Coins!{loading ? "" : ` (${coins.length})`}</h1>
       {loading ? (
         <strong>Loading...</strong>
       ) : (
-        <ul>
-          {coins.map((coin) => {
-            return (
-              <li key={coin.id}>
-                {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD
-              </li>
-            );
-          })}
-        </ul>
+        <div>
+          <select onChange={onChangeSelect}>
+            {coins.map((coin) => {
+              return (
+                <option key={coin.id} value={coin.id}>
+                  {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD
+                </option>
+              );
+            })}
+          </select>
+          <input
+            value={usd}
+            onChange={onChangeUsd}
+            type="number"
+            placeholder="input usd"
+          />
+          {"USD "} <br />
+          {usd === "" ? null : (
+            <div>
+              {usd /
+                coins.find((coin) => coin.id === selectedCoin).quotes.USD.price}
+              &nbsp;{coins.find((coin) => coin.id === selectedCoin).symbol}
+            </div>
+          )}
+        </div>
       )}
     </div>
   );
